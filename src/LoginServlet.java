6e7c7091c6ea288b804eb87b1e5305e51afbdca7
xibeijia <xibeijia@xibeijia-PC>
2019-06-21 23:33:12 +0800

    j2ee

---
+++ src/LoginServlet.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String name = req.getParameter("name");
+		String password = req.getParameter("password");
+		
+		System.out.println("name:"+name);
+		System.out.println("password:"+password);
+	}
+	
+}
