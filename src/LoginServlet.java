e5de9210471901cfe881efc454d4821ff9e78dff
xibeijia <xibeijia@xibeijia-PC>
2019-06-23 16:48:07 +0800

    upload

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,13 +8,31 @@ import javax.servlet.http.HttpServletResponse;
 
 public class LoginServlet extends HttpServlet {
 
+	private static final long serialVersionUID = 1L;
+	
+	public LoginServlet() {
+		// TODO Auto-generated constructor stub
+		System.out.println("LoginServlet 构造方法 被调用");
+	}
+	public void init(ServletConfig config) {
+        System.out.println("init(ServletConfig)");
+    }
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("UTF-8");
 		String name = req.getParameter("name");
 		String password = req.getParameter("password");
 		
-		String html = null;
+		if ("admin".equals(name) && "123".equals(password)) {
+            req.getRequestDispatcher("success.html").forward(req, resp);
+        }else{
+            
+        	resp.setStatus(301);
+            resp.setHeader("Location", "fail.html");
+        	/*resp.sendRedirect("fail.html");*/
+        }
+		
+		/*String html = null;
 		if ("admin".equals(name) && "123".equals(password)) {
 			html = "<div style='color:green'>登录成功</div>";
 		} else {
@@ -23,7 +41,7 @@ public class LoginServlet extends HttpServlet {
 		
 		resp.setContentType("text/html; charset=UTF-8");
 		PrintWriter pw = resp.getWriter();
-		pw.println(html);
+		pw.println(html);*/
 		
 		System.out.println("name:"+name);
 		System.out.println("password:"+password);
